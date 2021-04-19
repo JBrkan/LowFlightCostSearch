1a6af459d66855122bd2f0394f48d158ff36d679
Josip Brkan <brkan34@gmail.com>
2021-04-19 14:53:50 +0200

    Exception adding?

---
+++ src/main/java/com/jbrkan/lowflightcostapp/Controllers/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,4 @@
+package com.jbrkan.lowflightcostapp.Controllers;
+
+public class RestResponseEntityExceptionHandler {
+}
---
+++ src/main/java/com/jbrkan/lowflightcostapp/Exceptions/TokenNotFoundException.java
@@ -0,0 +1,4 @@
+package com.jbrkan.lowflightcostapp.Exceptions;
+
+public class TokenExpiredException {
+}
