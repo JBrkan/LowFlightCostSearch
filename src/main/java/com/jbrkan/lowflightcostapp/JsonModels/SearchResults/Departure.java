1c752f9fc918891efbf441bba74d6175e7eb0c89
Josip Brkan <brkan34@gmail.com>
2021-04-19 14:19:11 +0200

    Daljna izrada, rip vrijeme

---
+++ src/main/java/com/jbrkan/lowflightcostapp/JsonModels/SearchResults/Departure.java
@@ -0,0 +1,4 @@
+package com.jbrkan.lowflightcostapp.JsonModels.SearchResults;
+
+public class Departure {
+}
---
+++ src/main/java/com/jbrkan/lowflightcostapp/JsonModels/SearchResults/Itineraries.java
@@ -0,0 +1,15 @@
+package com.jbrkan.lowflightcostapp.JsonModels.SearchResults;
+
+import javax.swing.text.Segment;
+import java.util.List;
+
+public class Itinerary {
+    List<Segments> segments;
+
+    public Itinerary() {
+    }
+
+    public List<Segments> getSegments() {
+        return segments;
+    }
+}
